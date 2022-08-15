0bd96f56fb730dfc2ec4fb2d7f998e9b2755d3b3
Mark Wilson <69532317+wilsonERR@users.noreply.github.com>
2022-08-15 10:49:34 -0500

    Added save feature to AddPart

--- src/main/java/com/mwilson/inventoryfx/AddPart.java
+++ src/main/java/com/mwilson/inventoryfx/AddPart.java
@@ -5,12 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -41,18 +39,6 @@ public class AddPart implements Initializable {
         machineIdCompanyNameLabel.setText("Company Name");
     }
 
-    public void OnSaveClicked(ActionEvent actionEvent) {
-        if (inHouseRadio.isSelected()){
-
-        }
-        else if (outsourcedRadio.isSelected()) {
-
-        }
-        else {
-            System.out.println("You broke my radio!");
-        }
-    }
-
     public void OnCancelClicked(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
@@ -62,11 +48,63 @@ public class AddPart implements Initializable {
         stage.show();
     }
 
-    public void saveInHousePart(){
+    public static int getNewID(){
+        int id = 1;
+        for (int i = 0; i < Inventory.getAllParts().size(); i++){
+            id++;
+        }
+        return id;
 
     }
 
-    public void saveOutsourcedPart(){
+    public void OnSaveClicked(ActionEvent actionEvent) {
+        try {
+            int inventory = Integer.parseInt(fieldInv.getText());
+            int min = Integer.parseInt(fieldMin.getText());
+            int max = Integer.parseInt(fieldMax.getText());
+            if (max < min){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Error!");
+                alert.setHeaderText("Input Error!");
+                alert.setContentText("Part minimum must be less than maximum");
+                alert.showAndWait();
+            }
+            else if (inventory < min || inventory > max){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Error!");
+                alert.setHeaderText("Input Error!");
+                alert.setContentText("Part inventory must be between minimum and maximum");
+                alert.showAndWait();
+            }
+            else {
+                int id = getNewID();
+                String name = fieldName.getText();
+                double price = Double.parseDouble(fieldCost.getText());
+                if (inHouseRadio.isSelected()){
+                    int machId = Integer.parseInt(fieldMachineIdCompanyName.getText());
+                    InHouse inHouse = new InHouse(id, name, price, inventory, min, max, machId);
+                    Inventory.addPart(inHouse);
+                }
+                else {
+                    String coName = fieldMachineIdCompanyName.getText();
+                    Outsourced outsourced = new Outsourced(id, name, price, inventory, min, max, coName);
+                    Inventory.addPart(outsourced);
+                }
+                Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+                Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setTitle("Inventory Management System");
+                stage.setScene(scene);
+                stage.show();
+
+            }
+        } catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error!");
+            alert.setHeaderText("Input Error!");
+            alert.setContentText("Inventory, Cost, Min, Max, and Machine ID fields must contain numerical values");
+            alert.showAndWait();
+        }
 
     }
 }
